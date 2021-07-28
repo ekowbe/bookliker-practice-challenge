2e2694d834b7cf55f17ee23bcb151fccddd91428
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-07-28 13:00:37 +0000

    deliverable one begun

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const URL_PREFIX = 'http://localhost:3000/';
+
+document.addEventListener("DOMContentLoaded", function() {
+    // get list of books and render them
+
+    fetch(URL_PREFIX + 'books')
+        .then(rsp => rsp.json())
+        .then(books => {
+            console.log(books)
+            // get the div
+            const bookListPanel = document.querySelector('#list-panel')
+            const bookList = bookListPanel.ul
+            bookListPanel.innerHTML = ''
+
+            // populate with books
+            for (let i = 0; i < books.length; i++) {
+                const book = books[i];
+                
+            }
+        })
+});
